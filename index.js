1f84589ea4d2902ea5acdf0a92c09aeaa908242d
Mignot-m <mignotmihiret8@gmail.com>
2024-07-27 21:06:04 +0300

    third amazon-api

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
-const stripe = require("stripe")(process.env.STRIPE_KEY);
+const stripe = require("stripe")(
+  "sk_test_51PeEwsRwMrYq4qXCOJccKNnvEyGSU6zBHWQPbWfXLAJLMmLst0fny5CQWSgSYNyr84J9ZEEEJuaxbNIfqHAC9NOz007d3s1JaE"
+);
 const app = express();
-app.use(cors({ origin: true }));
+app.use(cors());
 
 app.use(express.json());
 app.get("/", (req, res) => {
